ee38cf3d324f8a1b5d7abea290917d3ac7bf0d2e
Brett Shollenberger <brett.shollenberger@gmail.com>
2013-12-09 14:30:53 -0500

    Ensure idempotence of singularize() & pluralize() methods
    "system".pluralize() == "system".pluralize().pluralize()
    
    "system".singularize() returns "system," not undefined

--- spec/inflector/inflector.spec.js
+++ spec/inflector/inflector.spec.js
@@ -15,41 +15,92 @@ describe('Inflector', function() {
   });
 
   describe('String#isEmpty', function() {
-    it('returns true if the string has a length of zero', function() {
+    it('returns false if the string has a length of more than one', function() {
       expect('hi'.isEmpty()).toEqual(false);
+    });
+
+    it('returns true if the string has a length of exactly zero', function() {
       expect(''.isEmpty()).toEqual(true);
     });
   });
 
   describe('String#pluralize', function() {
 
-    it('pluralizes words', function() {
+    it('pluralizes standard English ends-in-s', function() {
       expect('post'.pluralize()).toEqual('posts');
-      expect('greens'.pluralize()).toEqual('greens');
+      expect('green'.pluralize()).toEqual('greens');
+    });
+
+    it('pluralizes -is to -es', function() {
       expect('axis'.pluralize()).toEqual('axes');
       expect('testis'.pluralize()).toEqual('testes');
+    });
+
+    it('pluralizes -us to -i', function() {
       expect('octopus'.pluralize()).toEqual('octopi');
+    });
+
+    it('pluralizes -sis to -ses', function() {
       expect('diagnosis'.pluralize()).toEqual('diagnoses');
       expect('analysis'.pluralize()).toEqual('analyses');
       expect('virus'.pluralize()).toEqual('viruses');
       expect('status'.pluralize()).toEqual('statuses');
+    });
+
+    it('pluralizes -ias to -ses', function() {
       expect('alias'.pluralize()).toEqual('aliases');
+    });
+
+    it('gets the special pluralization of "bus"', function() {
       expect('bus'.pluralize()).toEqual('buses');
+    });
+
+    it('pluralizes -o to -oes', function() {
       expect('buffalo'.pluralize()).toEqual('buffaloes');
       expect('tomato'.pluralize()).toEqual('tomatoes');
-      expect('tium'.pluralize()).toEqual('tia');
-      expect('tia'.pluralize()).toEqual('tia');
+    });
+
+    it('pluralizes -tium to -tia', function() {
+      expect('consortium'.pluralize()).toEqual('consortia');
+      expect('hospitium'.pluralize()).toEqual('hospitia');
+    });
+
+    it('pluralizes -fe to -ives', function() {
       expect('fife'.pluralize()).toEqual('fives');
+    });
+
+    it('pluralizes -ve to -ves', function() {
       expect('hive'.pluralize()).toEqual('hives');
+    });
+
+    it('pluralizes -quy to -quies', function() {
       expect('soliloquy'.pluralize()).toEqual('soliloquies');
+    });
+
+    it('pluralizes -ex to -exes', function() {
       expect('ex'.pluralize()).toEqual('exes');
+    });
+
+    it('pluralizes -ix to -ices', function() {
       expect('matrix'.pluralize()).toEqual('matrices');
+    });
+
+    it('pluralizes -ex to -ices', function() {
       expect('vertex'.pluralize()).toEqual('vertices');
       expect('index'.pluralize()).toEqual('indices');
+    });
+
+    it('pluralizes -ouse to -ice', function() {
       expect('mouse'.pluralize()).toEqual('mice');
       expect('louse'.pluralize()).toEqual('lice');
+    });
+
+    it('pluralizes ox/oxen appropriately', function() {
       expect('ox'.pluralize()).toEqual('oxen');
       expect('oxen'.pluralize()).toEqual('oxen');
+    });
+
+    it('pluralizes irregular words', function() {
       expect('quiz'.pluralize()).toEqual('quizzes');
       expect('person'.pluralize()).toEqual('people');
       expect('man'.pluralize()).toEqual('men');
@@ -59,6 +110,9 @@ describe('Inflector', function() {
       expect('move'.pluralize()).toEqual('moves');
       expect('cow'.pluralize()).toEqual('cows');
       expect('zombie'.pluralize()).toEqual('zombies');
+    });
+
+    it('does not pluralize uncountables', function() {
       expect('equipment'.pluralize()).toEqual('equipment');
       expect('information'.pluralize()).toEqual('information');
       expect('rice'.pluralize()).toEqual('rice');
@@ -76,43 +130,105 @@ describe('Inflector', function() {
       expect('chicken'.pluralize(0)).toEqual('chickens');
     });
 
+    describe('idempotence', function() {
+      it('has the same effect many times over as calling it only once', function() {
+        expect('quiz'.pluralize().pluralize().pluralize().pluralize()).toEqual('quizzes');
+      });
+
+      it('returns the plural if passed the plural', function() {
+        expect('systems'.pluralize()).toEqual('systems');
+      });
+    });
+
   });
 
   describe('String#singularize', function() {
 
-    it('singularizes words', function() {
+    it('singularizes -s to no -s', function() {
       expect('posts'.singularize()).toEqual('post');
+    });
+
+    it('singularizes -ness to -ness', function() {
       expect('sweetness'.singularize()).toEqual('sweetness');
+    });
+
+    it('singularizes -es to no -es', function() {
       expect('sweetnesses'.singularize()).toEqual('sweetness');
+    });
+
+    it('handles uncountables', function() {
       expect('news'.singularize()).toEqual('news');
-      expect('tia'.singularize()).toEqual('tium');
+      expect('series'.singularize()).toEqual('series');
+    });
+
+    it('singularizes -tia to -tium', function() {
+      expect('consortia'.singularize()).toEqual('consortium');
+    });
+
+    it('singularizes -tia to -tium', function() {
       expect('analyses'.singularize()).toEqual('analysis');
       expect('diagnoses'.singularize()).toEqual('diagnosis');
       expect('parantheses'.singularize()).toEqual('paranthesis');
       expect('prognoses'.singularize()).toEqual('prognosis');
       expect('synopses'.singularize()).toEqual('synopsis');
       expect('theses'.singularize()).toEqual('thesis');
+    });
+
+    it('singularizes -ves to -ve', function() {
       expect('fives'.singularize()).toEqual('fife');
       expect('hives'.singularize()).toEqual('hive');
-      expect('tives'.singularize()).toEqual('tive');
       expect('cloves'.singularize()).toEqual('clove');
+    });
+
+
+    it('singularizes -quies to -quy', function() {
       expect('soliloquies'.singularize()).toEqual('soliloquy');
-      expect('series'.singularize()).toEqual('series');
+    });
+
+    it('singularizes -ies to -ie', function() {
       expect('movies'.singularize()).toEqual('movie');
+    });
+
+    it('singularizes -ishes to -ish', function() {
       expect('fishes'.singularize()).toEqual('fish');
+    });
+
+    it('singularizes -ouse to -ice', function() {
       expect('lice'.singularize()).toEqual('louse');
-      expect('oreos'.singularize()).toEqual('oreo');
+    });
+
+    it('singularizes toes to toe', function() {
       expect('toes'.singularize()).toEqual('toe');
+    });
+
+
+    it('singularizes potatoes to potato', function() {
       expect('potatoes'.singularize()).toEqual('potato');
+    });
+
+    it('singularizes shoes to shoe', function() {
       expect('shoes'.singularize()).toEqual('shoe');
+    });
+
+    it('singularizes -ses and -es to -is', function() {
       expect('crises'.singularize()).toEqual('crisis');
       expect('testes'.singularize()).toEqual('testis');
-      expect('axes'.singularize()).toEqual('axis');
-      expect('octopi'.singularize()).toEqual('octopus');
       expect('aliases'.singularize()).toEqual('alias');
-      expect('oxen'.singularize()).toEqual('ox');
+      expect('axes'.singularize()).toEqual('axis');
       expect('vertices'.singularize()).toEqual('vertex');
       expect('matrices'.singularize()).toEqual('matrix');
+    });
+
+    it('singularize -i to -us', function() {
+      expect('octopi'.singularize()).toEqual('octopus');
+    });
+    
+    it('singularizes oxen/ox', function() {
+      expect('oxen'.singularize()).toEqual('ox');
+      expect('ox'.singularize()).toEqual('ox');
+    });
+
+    it('singularizes irregulars', function() {
       expect('quizzes'.singularize()).toEqual('quiz');
       expect('databases'.singularize()).toEqual('database');
       expect('people'.singularize()).toEqual('person');
@@ -123,6 +239,9 @@ describe('Inflector', function() {
       expect('moves'.singularize()).toEqual('move');
       expect('cows'.singularize()).toEqual('cow');
       expect('zombies'.singularize()).toEqual('zombie');
+    });
+
+    it('leaves uncountables alone', function() {
       expect('equipment'.singularize()).toEqual('equipment');
       expect('information'.singularize()).toEqual('information');
       expect('rice'.singularize()).toEqual('rice');
@@ -132,6 +251,16 @@ describe('Inflector', function() {
       expect('sheep'.singularize()).toEqual('sheep');
       expect('jeans'.singularize()).toEqual('jeans');
       expect('police'.singularize()).toEqual('police');
+    });
+
+    describe('idempotence', function() {
+      it('has the same effect no matter how many times it is called', function() {
+        expect('systems'.singularize().singularize().singularize()).toEqual('system');
+      });
+
+      it('will return the singular if passed the singular (same as idempotence)', function() {
+        expect('system'.singularize()).toEqual('system');
+      });
     });
 
   });
